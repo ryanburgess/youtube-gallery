dd7a462432a7d7d92006e8be60ef832530624809
Ryan <rburgess@evernote.com>
2014-03-01 11:03:37 -0800

    updates to video gallery
    Change-Id: Ibc49fdc353c4bc6b5670f1a71ad1c3efae7cfe5c

--- js/video-gallery.js
+++ js/video-gallery.js
@@ -1,24 +1,78 @@
 (function($) {
     "use strict";
+
+    var query = "(-webkit-min-device-pixel-ratio: 1.5),\
+    (min--moz-device-pixel-ratio: 1.5),\
+    (-o-min-device-pixel-ratio: 3/2),\
+    (min-device-pixel-ratio: 1.5),\
+    (min-resolution: 144dpi),\
+    (min-resolution: 1.5dppx)";
     
+   // var ytThumbs = function(vidId){
     function ytThumbs(vidId){
         var imgUrl;
+        var vidTitle;
         $.ajax({
             type: "GET",
-                url: "http://gdata.youtube.com/feeds/api/videos/"+ videoID +"?v=2&alt=json",
+                url: "http://gdata.youtube.com/feeds/api/videos/"+ vidId +"?v=2&alt=json",
                 cache: false,
                 dataType:"jsonp",
                 success: function(result){
+                    //console.log(result)
                     // If retina screen use larger image
-                    if (window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia(query).matches)) {
+                   /* if (window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia(query).matches)) {
                         imgUrl = result.entry.media$group.media$thumbnail[1].url;
                     } else {
                         imgUrl = result.entry.media$group.media$thumbnail[0].url;
-                    }
-                    var vidTitle = result.entry.title.$t;
-                   // $(".more-learn-videos a[href='" + video +"']").html("<img src='"+ imgUrl +"' alt='"+ vidTitle +" Thumbnail' height='180' /><span class='icon' lang='en'>Play</span>");
+                    }*/
+                    vidTitle = result.entry.title.$t;
+                    vidId(vidTitle);
+
               }
         });
+        
+        //return vidTitle;
     }
+
+    function test(value){
+        var newvalue = value;
+        return newvalue;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "video.json",
+        dataType: "json",
+        cache: false,
+        success: function(data){
+            var videos = data.videos;
+            var html = "";
+            console.log(data)
+
+            for (var i = 0; i < videos.length; i++){
+                var section = videos[i].section;
+                var video_id = videos[i].video_id;
+
+                html += "<h2>"+ section +"</h2>";
+                html+= "<ul>";
+
+                for(var a = 0; a < videos[i].video_id.length; a++){
+                   var vidId = videos[i].video_id[a];
+                   //var videoData = ytThumbs(vidId);
+                   //var videoTitle = videoData.title;
+                   var videoData = ytThumbs(vidId);
+                   var videoTitle = videoData;
     
+                   console.log("test " + videoTitle)
+                   html += "<li>"+ "TEST " + videoTitle +"</li>";
+                }
+
+                html += "</ul>";
+                
+            }
+            // add videos to html
+            $(".videos").html(html);
+
+        }
+    });
 }(jQuery));
