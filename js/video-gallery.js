eaefebcd04f7b501989e0b337238d1c95fa3d0f3
Ryan <rburgess@evernote.com>
2014-03-01 11:56:18 -0800

    add styling
    Change-Id: I42c15015bd573d46302e98b990bc4683c521d5a3

--- js/video-gallery.js
+++ js/video-gallery.js
@@ -17,15 +17,15 @@
                 cache: false,
                 dataType:"jsonp",
                 success: function(result){
-                    //console.log(result)
                     // If retina screen use larger image
-                   /* if (window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia(query).matches)) {
+                    if (window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia(query).matches)) {
                         imgUrl = result.entry.media$group.media$thumbnail[1].url;
                     } else {
                         imgUrl = result.entry.media$group.media$thumbnail[0].url;
-                    }*/
+                    }
                     vidTitle = result.entry.title.$t;
-                    vidId(vidTitle);
+                    // push video thumbnails
+                    $("."+section).append("<li><a href='https://www.youtube.com/embed/"+ vidId +"'' class='video-thumbnail'><img src='"+ imgUrl +"' alt='"+ vidTitle +" Thumbnail' /><span class='icon' lang='en'>Play</span></a></li>");
 
               }
         });
@@ -45,18 +45,17 @@
                 var section = videos[i].section;
                 var video_id = videos[i].video_id;
 
-                html += "<h2>"+ section +"</h2>";
-                html+= "<ul>";
+                var className = section.toLowerCase();
+                className = className.replace(" ", "-");
+
+                var sectionTitle = section.charAt(0).toUpperCase() + section.slice(1);
+
+                html += "<h2>"+ sectionTitle +"</h2>";
+                html+= "<ul class='"+ className +"'>";
 
                 for(var a = 0; a < videos[i].video_id.length; a++){
                    var vidId = videos[i].video_id[a];
-                   //var videoData = ytThumbs(vidId);
-                   //var videoTitle = videoData.title;
-                   var videoData = ytThumbs(vidId);
-                   var videoTitle = videoData;
-    
-                   console.log("test " + videoTitle)
-                   html += "<li>"+ "TEST " + videoTitle +"</li>";
+                   ytThumbs(section, vidId)
                 }
 
                 html += "</ul>";
@@ -67,4 +66,78 @@
 
         }
     });
+
+    //modal windows
+    $("body").on("click", ".video-thumbnail", function(e) {
+        //load modal mask
+        $("<div id='mask'></div>").appendTo("body");
+        
+        //remove existing modal window
+        $("#modal-window").remove();
+
+        var content;
+
+        //Checks if href points to media or a div that is defined in the DOM
+        if ($(this).attr("href").match("^#")) {
+            var contentID = $(this).attr("href");
+            content = $(contentID).html();
+        } else {
+            content = $(this).attr("href");
+        }
+        var vidParameters = "?hd=1&autoplay=1&showinfo=0";
+        //if youtube video or images passed into modal
+        if (content.indexOf("youtube.com/embed/") !== -1){
+            content = "<iframe src='" + content + vidParameters + "' allowfullscreen='allowfullscreen'></iframe>";
+        } else if (content.indexOf("jpg") !== -1 || content.indexOf("png") !== -1){
+            content = "<img src='"+ content +"'>";
+        }
+
+        //load modal window with content passed
+        $("<div id='modal-window' class='window'>" + content + "<a href='#' class='close'>X</a></div>").appendTo("body");
+
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+
+        $("#mask").css({"width": maskWidth, "height": maskHeight})
+                  .show();
+
+        $(".window").css("top", ($(window).height() - $(".window").height()) / 2 + $(window).scrollTop() + "px")
+                    .css("left", ($(window).width() - $(".window").width()) / 2 + $(window).scrollLeft() + "px")
+                    .fadeIn();
+
+        e.preventDefault();
+        return false;
+    });
+
+    var removeItems = function () {
+        $("#mask, .window").fadeOut();
+        $(".window iframe").remove();
+        setTimeout(function () {
+            $("#mask, .window").remove();
+        }, 500);
+    };
+
+    //if close button is clicked
+    $("body").on("click", ".window .close", function (e) {
+        //Cancel the link behavior
+        e.preventDefault();
+        removeItems();
+    });
+
+    //if mask is clicked
+    $("body").on("click", "#mask", function (e) {
+        //Cancel the link behavior
+        e.preventDefault();
+        removeItems();
+    });
+
+    // Change modal window size repsonsive
+    function checkWindowSize() {
+        $(".window").css("top", ($(window).height() - $(".window").height()) / 2 + $(window).scrollTop() + "px")
+                    .css("left", ($(window).width() - $(".window").width()) / 2 + $(window).scrollLeft() + "px");
+        $("#mask").css({"width":    $(window).width(), "height": $(document).height()});
+    }
+    $(window).bind("resize", checkWindowSize);
+
+
 }(jQuery));
